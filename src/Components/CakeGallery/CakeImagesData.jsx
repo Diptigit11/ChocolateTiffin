f7a66446f8d7bf570ac02173b698eceb7c2bbdbe
Dipti <singh236dipti@gmail.com>
2024-07-07 20:53:31 +0530

    update icon

--- src/Components/CakeGallery/CakeImagesData.jsx
+++ src/Components/CakeGallery/CakeImagesData.jsx
@@ -12,50 +12,7 @@ import image9 from '/img/caramel.jpeg'
 
 
 export const CakeImageData = [
-  {
-    id: 1,
-    src: "https://www.fnp.com/images/pr/l/v200/black-forest-cake-half-kg_1.jpg",
-    caption: "Black Forest Cake",
-    name: "Black Forest Cake",
-    description: "A Black Forest cake is a delicious dessert with layers of chocolate cake, whipped cream, and cherries. It's topped with more whipped cream, chocolate shavings, and cherries, making it a tasty and pretty treat.",
-    weightOptions: [
-      { weight: "250g", price: 180 },
-      { weight: "1/2 kg", price: 300 },
-      { weight: "1 kg", price: 600 },
-      { weight: "1.5 kg", price: 900 }
-    ],
-    reviews: 16,
-    rating: 5
-  },
-  {
-    id: 2,
-    src: image1,
-    caption: "Rose Falooda Cake",
-    name: "Rose Falooda Cake",
-    description: "A delightful cake with layers of soft sponge infused with rose syrup, topped with a creamy falooda mixture. The cake is decorated with nuts, giving it a fragrant and elegant touch.",
-    weightOptions: [
-      { weight: "250g", price: 200 },
-      { weight: "1/2 kg", price: 350 },
-      { weight: "1 kg", price: 700 },
-      { weight: "1.5 kg", price: 1050 }
-    ],
-    reviews: 10,
-    rating: 4
-  },
-  {
-    id: 3,
-    src: image2,
-    caption: "Mango Cake",
-    name: "Mango Delight Cake",
-    description: "This tropical cake features layers of moist mango-flavored sponge and smooth mango cream. It's topped with fresh mango slices and a glossy mango glaze, perfect for mango lovers.",
-    weightOptions: [
-      { weight: "1/2 kg", price: 400 },
-      { weight: "1 kg", price: 800 },
-      { weight: "1.5 kg", price: 1200 }
-    ],
-    reviews: 10,
-    rating: 4
-  },
+
   {
     id: 4,
     src: "https://www.fnp.com/images/pr/l/v20221214202702/chocolaty-truffle-cake-half-kg_1.jpg",
@@ -136,6 +93,50 @@ export const CakeImageData = [
       { weight: "1/2 kg", price: 450 },
       { weight: "1 kg", price: 900 },
       { weight: "1.5 kg", price: 1300 }
+    ],
+    reviews: 10,
+    rating: 4
+  },
+  {
+    id: 1,
+    src: "https://www.fnp.com/images/pr/l/v200/black-forest-cake-half-kg_1.jpg",
+    caption: "Black Forest Cake",
+    name: "Black Forest Cake",
+    description: "A Black Forest cake is a delicious dessert with layers of chocolate cake, whipped cream, and cherries. It's topped with more whipped cream, chocolate shavings, and cherries, making it a tasty and pretty treat.",
+    weightOptions: [
+      { weight: "250g", price: 180 },
+      { weight: "1/2 kg", price: 300 },
+      { weight: "1 kg", price: 600 },
+      { weight: "1.5 kg", price: 900 }
+    ],
+    reviews: 16,
+    rating: 5
+  },
+  {
+    id: 2,
+    src: image1,
+    caption: "Rose Falooda Cake",
+    name: "Rose Falooda Cake",
+    description: "A delightful cake with layers of soft sponge infused with rose syrup, topped with a creamy falooda mixture. The cake is decorated with nuts, giving it a fragrant and elegant touch.",
+    weightOptions: [
+      { weight: "250g", price: 200 },
+      { weight: "1/2 kg", price: 350 },
+      { weight: "1 kg", price: 700 },
+      { weight: "1.5 kg", price: 1050 }
+    ],
+    reviews: 10,
+    rating: 4
+  },
+  {
+    id: 3,
+    src: image2,
+    caption: "Mango Cake",
+    name: "Mango Delight Cake",
+    description: "This tropical cake features layers of moist mango-flavored sponge and smooth mango cream. It's topped with fresh mango slices and a glossy mango glaze, perfect for mango lovers.",
+    weightOptions: [
+      { weight: "1/2 kg", price: 400 },
+      { weight: "1 kg", price: 800 },
+      { weight: "1.5 kg", price: 1200 }
     ],
     reviews: 10,
     rating: 4
