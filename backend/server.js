fe13cdce47da09e00cfb3db498cf104d804d0d7c
Dipti <singh236dipti@gmail.com>
2024-07-08 19:45:03 +0530

    added frontend url

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://chocolatetiffin.onrender.com',
   credentials: true,
 }));
 
